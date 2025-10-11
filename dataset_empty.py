edfaa4718ed3912019f428d805567059caead3aa
priyanshsingh-dev <dhanadpriyansh1234@gmail.com>
2025-10-11 15:59:15 +0530

    cleaned up

--- dataset_empty.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import json
-
-df=pd.read_csv("queries.csv")
-
-data=[{"query": row["query"], "json_output": []} for _, row in df.iterrows()]
-
-with open("dataset_empty.json", "w") as f:
-    json.dump(data, f, indent=2)
