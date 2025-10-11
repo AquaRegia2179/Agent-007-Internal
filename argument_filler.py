b66a569ef6e49cae582d35fe55ee31634d0aedb9
Soham-Kakkar <kakkarsoham@gmail.com>
2025-10-11 14:53:42 +0530

    Shortened prompt and corrected import errors

--- argument_filler.py
+++ argument_filler.py
@@ -1,6 +1,6 @@
 import json
 import time
-import loadModel
+from loadModel import loadModel
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
