44ab07fb16eb13c06d50fa79d220587c848b97e0
Soham-Kakkar <kakkarsoham@gmail.com>
2025-10-11 14:40:19 +0530

    Corrected import errors

--- loadModel.py
+++ loadModel.py
@@ -1,3 +1,4 @@
+import os
 model = "gemini"
 
 def loadModel():
@@ -6,4 +7,4 @@ def loadModel():
         return ChatGoogleGenerativeAI(model='gemini-2.5-pro', temperature=0, google_api_key=os.getenv("GOOGLE_API_KEY"))
     elif model == "mistral":
         from langchain_mistralai.chat_models import ChatMistralAI
-        return ChatMistralAI(api_key=API_KEY, model="mistral-large-latest")
+        return ChatMistralAI(api_key=os.getenv("MISTRAL_API_KEY"), model="mistral-large-latest")
